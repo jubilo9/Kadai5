780262983a630f1c5e82cf4cd5e7a9d3b2af3c07
jubilo9 <g13944smgit@gm.tsuda.ac.jp>
2015-04-13 14:41:50 +0900

    HelloWorldです。4/13

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO code application logic here
+		System.out.println("hello world");
+		System.out.println("‚±‚ñ‚É‚¿‚Í¢ŠE");
+		System.out.println(10+5);
+	}
+}
